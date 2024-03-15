a19f263065196af4551d741d23a05a07ff0a013c
Samuel Gocník <samo1182@gmail.com>
2024-03-15 11:54:29 +0100

    feat: add CreatedAt and DeletedAt to baseentity

--- FundParser/FundParser/Migrations/20240315105021_AddCreatedAtAndDeletedAt.Designer.cs
+++ FundParser/FundParser/Migrations/20240315105021_AddCreatedAtAndDeletedAt.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FundParser.Migrations
 {
     [DbContext(typeof(FundParserDbContext))]
-    partial class FundParserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240315105021_AddCreatedAtAndDeletedAt")]
+    partial class AddCreatedAtAndDeletedAt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.2");
