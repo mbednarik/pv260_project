54092f64e07549a69c80e1474bfdb41bff7eae19
Martin Bednarik <mbednarik8892@gmail.com>
2024-03-16 19:43:37 +0100

    fix: remove redundant try catch

--- FundParser/BL/Services/CompanyService/CompanyService.cs
+++ FundParser/BL/Services/CompanyService/CompanyService.cs
@@ -19,29 +19,21 @@ public class CompanyService : ICompanyService
 
     public async Task<CompanyDTO?> AddCompanyIfNotExists(AddCompanyDTO company)
     {
-        try
+        // Cusip should be unique
+        var existingCompany = await _uow.CompanyRepository.GetQueryable()
+            .FirstOrDefaultAsync(f => f.Cusip == company.Cusip);
+        if (existingCompany != null)
         {
-            // Cusip should be unique
-            var existingCompany = await _uow.CompanyRepository.GetQueryable()
-                .FirstOrDefaultAsync(f => f.Cusip == company.Cusip);
-            if (existingCompany != null)
-            {
-                return _mapper.Map<CompanyDTO>(existingCompany);
-            }
+            return _mapper.Map<CompanyDTO>(existingCompany);
+        }
 
-            var newCompany = _uow.CompanyRepository.Insert(new Company
-            {
-                Cusip = company.Cusip,
-                Ticker = company.Ticker,
-                Name = company.Name,
-            });
-            await _uow.CommitAsync();
-            return _mapper.Map<CompanyDTO>(newCompany);     
-        }
-        catch (Exception e)
+        var newCompany = _uow.CompanyRepository.Insert(new Company
         {
-            Console.WriteLine("AddCompanyIfNotExists error: {0}", e.Message);
-            return null;
-        }
+            Cusip = company.Cusip,
+            Ticker = company.Ticker,
+            Name = company.Name,
+        });
+        await _uow.CommitAsync();
+        return _mapper.Map<CompanyDTO>(newCompany);
     }
 }
