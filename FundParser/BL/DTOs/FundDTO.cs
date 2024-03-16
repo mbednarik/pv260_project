ce1bb7a9f4288651dbc9ed7b5e9f2925be2246b1
Martin Bednarik <mbednarik8892@gmail.com>
2024-03-16 19:42:31 +0100

    fix: uncomment variable

--- FundParser/BL/DTOs/FundDTO.cs
+++ FundParser/BL/DTOs/FundDTO.cs
@@ -5,7 +5,7 @@ public class FundDTO
     public int Id { get; set; }
     public string Name { get; set; }
 
-    // public ICollection<HoldingDTO> Holdings { get; set; }
+    public ICollection<HoldingDTO> Holdings { get; set; }
 
     // TODO: Add HoldingDiffDTO
     // public virtual ICollection<HoldingDiff> HoldingDiffs { get; set; }
