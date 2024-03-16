735e7992b8a2ae8f5517054050bed498d3e6e2a0
Michal Adler <129653243+adlermichal@users.noreply.github.com>
2024-03-17 00:40:36 +0100

    feat: Add endpoint for obtaining holding diffs (#11)

---
+++ FundParser/FundParser/Controllers/HoldingDiffController.cs
@@ -0,0 +1,25 @@
+﻿using BL.DTOs;
+using BL.Services.HoldingDiffService;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FundParser.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HoldingDiffController : ControllerBase
+    {
+        private readonly IHoldingDiffService holdingDiffService;
+
+        public HoldingDiffController(IHoldingDiffService holdingDiffService)
+        {
+            this.holdingDiffService = holdingDiffService;
+        }
+
+        [HttpGet(Name = nameof(GetHoldingDiffs))]
+        public Task<IEnumerable<HoldingDiffDTO>> GetHoldingDiffs(int fundId, DateTime oldHoldingDate, DateTime newHoldingDate, CancellationToken cancellationToken)
+        {
+            return holdingDiffService.GetHoldingDiffs(fundId, oldHoldingDate, newHoldingDate, cancellationToken);
+        }
+    }
+}
