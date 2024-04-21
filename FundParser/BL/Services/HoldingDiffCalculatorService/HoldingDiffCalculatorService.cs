806e9fd999aa0978507ac773027ebf93864132be
Michal Adler <adler.michal@seznam.cz>
2024-04-21 22:59:47 +0200

    fix: fix formatting

--- FundParser/BL/Services/HoldingDiffCalculatorService/HoldingDiffCalculatorService.cs
+++ FundParser/BL/Services/HoldingDiffCalculatorService/HoldingDiffCalculatorService.cs
@@ -11,66 +11,66 @@ public class HoldingDiffCalculatorService : IHoldingDiffCalculatorService
 
         return CompareHoldings(oldHoldingsList, newHoldingsList);
     }
-        
-    private IEnumerable<HoldingDiff> CompareHoldings(IReadOnlyCollection<Holding> oldHoldings, IReadOnlyCollection<Holding> newHoldings)
-        {
-            var oldHoldingDict = oldHoldings.ToDictionary(h => h.CompanyId);
-            var newHoldingDict = newHoldings.ToDictionary(h => h.CompanyId);
 
-            var holdingDiffs = oldHoldings
-                .Select(h =>
-                {
-                    var newHolding = newHoldingDict.GetValueOrDefault(h.CompanyId);
+    private static IEnumerable<HoldingDiff> CompareHoldings(IReadOnlyCollection<Holding> oldHoldings, IReadOnlyCollection<Holding> newHoldings)
+    {
+        var oldHoldingDict = oldHoldings.ToDictionary(h => h.CompanyId);
+        var newHoldingDict = newHoldings.ToDictionary(h => h.CompanyId);
 
-                    return newHolding == null ? GetOldHoldingDiff(h) : GetHoldingDiff(h, newHolding);
-                })
-                .Concat(newHoldings
-                    .Where(h => !oldHoldingDict.ContainsKey(h.CompanyId))
-                    .Select(GetNewHoldingDiff));
-
-            return holdingDiffs;
-        }
-
-        private static HoldingDiff GetNewHoldingDiff(Holding newHolding)
-        {
-            return new HoldingDiff
+        var holdingDiffs = oldHoldings
+            .Select(h =>
             {
-                FundId = newHolding.FundId,
-                CompanyId = newHolding.CompanyId,
-                NewHoldingId = newHolding.Id,
-                OldShares = 0,
-                SharesChange = newHolding.Shares,
-                OldWeight = 0,
-                WeightChange = newHolding.Weight
-            };
-        }
+                var newHolding = newHoldingDict.GetValueOrDefault(h.CompanyId);
 
-        private static HoldingDiff GetOldHoldingDiff(Holding oldHolding)
-        {
-            return new HoldingDiff
-            {
-                FundId = oldHolding.FundId,
-                CompanyId = oldHolding.CompanyId,
-                OldHoldingId = oldHolding.Id,
-                OldShares = oldHolding.Shares,
-                SharesChange = -oldHolding.Shares,
-                OldWeight = oldHolding.Weight,
-                WeightChange = -oldHolding.Weight
-            };
-        }
+                return newHolding == null ? GetOldHoldingDiff(h) : GetHoldingDiff(h, newHolding);
+            })
+            .Concat(newHoldings
+                .Where(h => !oldHoldingDict.ContainsKey(h.CompanyId))
+                .Select(GetNewHoldingDiff));
 
-        private static HoldingDiff GetHoldingDiff(Holding oldHolding, Holding newHolding)
+        return holdingDiffs;
+    }
+
+    private static HoldingDiff GetNewHoldingDiff(Holding newHolding)
+    {
+        return new HoldingDiff
         {
-            return new HoldingDiff
-            {
-                FundId = oldHolding.FundId,
-                CompanyId = oldHolding.CompanyId,
-                OldHoldingId = oldHolding.Id,
-                NewHoldingId = newHolding.Id,
-                OldShares = oldHolding.Shares,
-                SharesChange = newHolding.Shares - oldHolding.Shares,
-                OldWeight = oldHolding.Weight,
-                WeightChange = newHolding.Weight - oldHolding.Weight
-            };
-        }
+            FundId = newHolding.FundId,
+            CompanyId = newHolding.CompanyId,
+            NewHoldingId = newHolding.Id,
+            OldShares = 0,
+            SharesChange = newHolding.Shares,
+            OldWeight = 0,
+            WeightChange = newHolding.Weight
+        };
+    }
+
+    private static HoldingDiff GetOldHoldingDiff(Holding oldHolding)
+    {
+        return new HoldingDiff
+        {
+            FundId = oldHolding.FundId,
+            CompanyId = oldHolding.CompanyId,
+            OldHoldingId = oldHolding.Id,
+            OldShares = oldHolding.Shares,
+            SharesChange = -oldHolding.Shares,
+            OldWeight = oldHolding.Weight,
+            WeightChange = -oldHolding.Weight
+        };
+    }
+
+    private static HoldingDiff GetHoldingDiff(Holding oldHolding, Holding newHolding)
+    {
+        return new HoldingDiff
+        {
+            FundId = oldHolding.FundId,
+            CompanyId = oldHolding.CompanyId,
+            OldHoldingId = oldHolding.Id,
+            NewHoldingId = newHolding.Id,
+            OldShares = oldHolding.Shares,
+            SharesChange = newHolding.Shares - oldHolding.Shares,
+            OldWeight = oldHolding.Weight,
+            WeightChange = newHolding.Weight - oldHolding.Weight
+        };
+    }
 }
