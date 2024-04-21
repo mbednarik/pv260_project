31aa783ca089ada24f66240ff92bb49e2be2c503
Samuel Gocník <samo1182@gmail.com>
2024-04-21 22:38:38 +0200

    fix: refactor FundCsvServiceTests

--- FundParser/FundParser.BL.Tests/Services/FundCsvServiceTests.cs
+++ FundParser/FundParser.BL.Tests/Services/FundCsvServiceTests.cs
@@ -14,125 +14,131 @@ namespace FundParser.BL.Tests.Services;
 [TestFixture]
 public class FundCsvServiceTests
 {
-    private const string Url = "http://supercoolwebsite.com/fund.csv";
-
-    private Mock<IHoldingService> _holdingServiceMock;
-    private Mock<ICsvParsingService<FundCsvRow>> _csvParsingServiceMock;
-    private Mock<IDownloaderService> _downloaderServiceMock;
-    private Mock<IConfiguration> _configurationMock;
-    private Mock<IConfigurationSection> _configurationSectionMock;
-    private Mock<IUnitOfWork> _unitOfWorkMock;
-    private FundCsvService _fundCsvService;
-
-    [SetUp]
-    public void SetUp()
+    public class UpdateHoldingsTests : FundCsvServiceTestsBase
     {
-        // Mock IConfigurationSection
-        _configurationSectionMock = new Mock<IConfigurationSection>();
-        _configurationSectionMock.SetupGet(m => m.Value).Returns(Url);
-
-        // Mock IConfiguration
-        _configurationMock = new Mock<IConfiguration>();
-        _configurationMock.Setup(m => m.GetSection(It.IsAny<string>())).Returns(_configurationSectionMock.Object);
-
-        // Mock other dependencies
-        _holdingServiceMock = new Mock<IHoldingService>();
-        _csvParsingServiceMock = new Mock<ICsvParsingService<FundCsvRow>>();
-        _downloaderServiceMock = new Mock<IDownloaderService>();
-        _unitOfWorkMock = new Mock<IUnitOfWork>();
-
-        // Initialize FundCsvService with mocked dependencies
-        _fundCsvService = new FundCsvService(
-            _holdingServiceMock.Object,
-            _csvParsingServiceMock.Object,
-            _downloaderServiceMock.Object,
-            _unitOfWorkMock.Object,
-            new Mock<ILoggingService>().Object,
-            _configurationMock.Object
-        );
-    }
-
-    [Test]
-    public void UpdateHoldings_DownloadFailed_ThrowsException()
-    {
-        // Arrange
-        _downloaderServiceMock.Setup(m => m.DownloadTextFileAsStringAsync(It.IsAny<string>(),
-                It.IsAny<List<(string, string)>>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync((string)null!);
-
-        // Act & Assert
-        Assert.Multiple(() =>
+        [Test]
+        public void UpdateHoldings_DownloadFailed_ThrowsException()
         {
-            Assert.That(() => _fundCsvService.UpdateHoldings(),
-                Throws.Exception.With.Message.EqualTo("Failed to download csv"));
-            Assert.That(() => _fundCsvService.UpdateHoldings(), Throws.Exception);
-        });
-    }
+            // Arrange
+            downloaderServiceMock.Setup(m => m.DownloadTextFileAsStringAsync(It.IsAny<string>(),
+                    It.IsAny<List<(string, string)>>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync((string)null!);
 
-    [Test]
-    public void UpdateHoldings_ParseFailed_ThrowsException()
-    {
-        // Arrange
-        const string csvString = "Fund,Cusip,Ticker,Company,Shares,MarketValue,Weight,Date\n" +
-                                 "ARKK,12345,ABC,Test Company,1000,$10000,10%,01/01/2022";
-        _downloaderServiceMock.Setup(m => m.DownloadTextFileAsStringAsync(It.IsAny<string>(),
-                It.IsAny<List<(string, string)>>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(csvString);
-        _csvParsingServiceMock.Setup(m => m.ParseString(It.IsAny<string>(), It.IsAny<CancellationToken>()))
-            .Returns((List<FundCsvRow>)null!);
-
-
-        // Act & Assert
-        Assert.Multiple(() =>
-        {
-            Assert.That(() => _fundCsvService.UpdateHoldings(),
-                Throws.Exception.With.Message.EqualTo("Failed to parse csv"));
-            Assert.That(() => _fundCsvService.UpdateHoldings(), Throws.Exception);
-        });
-    }
-
-    [Test]
-    public void UpdateHoldings_CsvUrlNotConfigured_ThrowsException()
-    {
-        // Arrange
-        _configurationSectionMock.SetupGet(m => m.Value).Returns((string)null!);
-
-        // Act & Assert
-        Assert.That(() => _fundCsvService.UpdateHoldings(), Throws.Exception);
-    }
-
-    [Test]
-    public async Task UpdateHoldings_SuccessfulUpdate_ReturnsCorrectCount()
-    {
-        // Arrange
-        const string testCsvString = "testCsvString";
-        _downloaderServiceMock.Setup(m => m.DownloadTextFileAsStringAsync(It.IsAny<string>(),
-                It.IsAny<List<(string, string)>>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(testCsvString);
-        _csvParsingServiceMock.Setup(m => m.ParseString(testCsvString, It.IsAny<CancellationToken>()))
-            .Returns(new List<FundCsvRow>
+            // Act & Assert
+            Assert.Multiple(() =>
             {
-                new FundCsvRow
-                {
-                    Fund = "ARKK",
-                    Cusip = "12345",
-                    Ticker = "ABC",
-                    Company = "Test Company",
-                    Shares = "1000",
-                    MarketValue = "$10000",
-                    Weight = "10%",
-                    Date = "01/01/2022"
-                }
+                Assert.That(() => fundCsvService.UpdateHoldings(),
+                    Throws.Exception.With.Message.EqualTo("Failed to download csv"));
+                Assert.That(() => fundCsvService.UpdateHoldings(), Throws.Exception);
             });
+        }
 
-        // Act
-        var result = await _fundCsvService.UpdateHoldings();
+        [Test]
+        public void UpdateHoldings_ParseFailed_ThrowsException()
+        {
+            // Arrange
+            const string csvString = "Fund,Cusip,Ticker,Company,Shares,MarketValue,Weight,Date\n" +
+                                     "ARKK,12345,ABC,Test Company,1000,$10000,10%,01/01/2022";
+            downloaderServiceMock.Setup(m => m.DownloadTextFileAsStringAsync(It.IsAny<string>(),
+                    It.IsAny<List<(string, string)>>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(csvString);
+            csvParsingServiceMock.Setup(m => m.ParseString(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .Returns((List<FundCsvRow>)null!);
 
-        // Assert
-        _holdingServiceMock.Verify(
-            holdingService => holdingService.AddHolding(It.IsAny<AddHoldingDTO>(), It.IsAny<CancellationToken>()),
-            Times.Once());
-        _unitOfWorkMock.Verify(uow => uow.CommitAsync(It.IsAny<CancellationToken>()), Times.Once());
-        Assert.That(result, Is.EqualTo(1));
+
+            // Act & Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(() => fundCsvService.UpdateHoldings(),
+                    Throws.Exception.With.Message.EqualTo("Failed to parse csv"));
+                Assert.That(() => fundCsvService.UpdateHoldings(), Throws.Exception);
+            });
+        }
+
+        [Test]
+        public void UpdateHoldings_CsvUrlNotConfigured_ThrowsException()
+        {
+            // Arrange
+            configurationSectionMock.SetupGet(m => m.Value).Returns((string)null!);
+
+            // Act & Assert
+            Assert.That(() => fundCsvService.UpdateHoldings(), Throws.Exception);
+        }
+
+        [Test]
+        public async Task UpdateHoldings_SuccessfulUpdate_ReturnsCorrectCount()
+        {
+            // Arrange
+            const string testCsvString = "testCsvString";
+            downloaderServiceMock.Setup(m => m.DownloadTextFileAsStringAsync(It.IsAny<string>(),
+                    It.IsAny<List<(string, string)>>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(testCsvString);
+            csvParsingServiceMock.Setup(m => m.ParseString(testCsvString, It.IsAny<CancellationToken>()))
+                .Returns(new List<FundCsvRow>
+                {
+                    new FundCsvRow
+                    {
+                        Fund = "ARKK",
+                        Cusip = "12345",
+                        Ticker = "ABC",
+                        Company = "Test Company",
+                        Shares = "1000",
+                        MarketValue = "$10000",
+                        Weight = "10%",
+                        Date = "01/01/2022"
+                    }
+                });
+
+            // Act
+            var result = await fundCsvService.UpdateHoldings();
+
+            // Assert
+            holdingServiceMock.Verify(
+                holdingService => holdingService.AddHolding(It.IsAny<AddHoldingDTO>(), It.IsAny<CancellationToken>()),
+                Times.Once());
+            unitOfWorkMock.Verify(uow => uow.CommitAsync(It.IsAny<CancellationToken>()), Times.Once());
+            Assert.That(result, Is.EqualTo(1));
+        }
+    }
+
+    public class FundCsvServiceTestsBase
+    {
+        private const string Url = "http://supercoolwebsite.com/fund.csv";
+
+        protected Mock<IHoldingService> holdingServiceMock;
+        protected Mock<ICsvParsingService<FundCsvRow>> csvParsingServiceMock;
+        protected Mock<IDownloaderService> downloaderServiceMock;
+        protected Mock<IConfiguration> configurationMock;
+        protected Mock<IConfigurationSection> configurationSectionMock;
+        protected Mock<IUnitOfWork> unitOfWorkMock;
+        protected FundCsvService fundCsvService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Mock IConfigurationSection
+            configurationSectionMock = new Mock<IConfigurationSection>();
+            configurationSectionMock.SetupGet(m => m.Value).Returns(Url);
+
+            // Mock IConfiguration
+            configurationMock = new Mock<IConfiguration>();
+            configurationMock.Setup(m => m.GetSection(It.IsAny<string>())).Returns(configurationSectionMock.Object);
+
+            // Mock other dependencies
+            holdingServiceMock = new Mock<IHoldingService>();
+            csvParsingServiceMock = new Mock<ICsvParsingService<FundCsvRow>>();
+            downloaderServiceMock = new Mock<IDownloaderService>();
+            unitOfWorkMock = new Mock<IUnitOfWork>();
+
+            // Initialize FundCsvService with mocked dependencies
+            fundCsvService = new FundCsvService(
+                holdingServiceMock.Object,
+                csvParsingServiceMock.Object,
+                downloaderServiceMock.Object,
+                unitOfWorkMock.Object,
+                new Mock<ILoggingService>().Object,
+                configurationMock.Object
+            );
+        }
     }
 }
